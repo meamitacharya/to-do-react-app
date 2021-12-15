cb0ecc69af1a1f8278cfdc9a5f67547d68bdbc40
Amit Acharya <amitacharya975@gmail.com>
2021-12-15 23:46:50 +0545

    :zap: refactored and react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,68 +1,22 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  //   Link,
+} from 'react-router-dom';
 
-import Form from './components/Form';
-import ToDoLists from './components/ToDoLists';
+import { HomePage } from './components/Homepage/HomePage';
+// import { Welcome } from './components/Welcome';
 
 function App() {
-  //States of application
-  const [inputText, setInputText] = useState('');
-  const [todos, setTodos] = useState([]);
-  const [status, setStatus] = useState('all');
-  const [filteredTodos, setFilteredTodos] = useState([]);
-
-  // This function runs only once when app is mounted in DOM
-  useEffect(() => {
-    async function fetchTodos() {
-      try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_API_BASE_URL}/api/todos`
-        );
-        setTodos(response.data.data);
-      } catch (err) {
-        console.log(err.message);
-      }
-    }
-
-    fetchTodos();
-  }, []);
-
-  // This effect runs when there is state changes in : Todos and Status
-  useEffect(() => {
-    const filterHandler = () => {
-      switch (status) {
-        case 'completed':
-          setFilteredTodos(todos.filter((todo) => todo.isCompleted === true));
-          break;
-        case 'uncompleted':
-          setFilteredTodos(todos.filter((todo) => todo.isCompleted === false));
-          break;
-        default:
-          setFilteredTodos(todos);
-      }
-    };
-    filterHandler();
-  }, [status, todos]);
-
   return (
-    <div className="App">
-      <header>
-        <h1>Amit's Todo List</h1>
-      </header>
-      <Form
-        todos={todos}
-        setTodos={setTodos}
-        inputText={inputText}
-        setInputText={setInputText}
-        setStatus={setStatus}
-      />
-      <ToDoLists
-        filteredTodos={filteredTodos}
-        todos={todos}
-        setTodos={setTodos}
-      />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
